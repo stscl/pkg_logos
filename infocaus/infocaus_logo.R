a09d13fe124a2092ff880cbbb9de76a5647472ee
SpatLyu <lyu.geosocial@gmail.com>
2026-03-15 00:00:01 +0800

    rename infocaus

---
+++ infocaus/infocaus_logo.R
@@ -0,0 +1,38 @@
+library(showtext)
+showtext_auto(enable = TRUE)
+font_add("ShineTypewriter", regular = "./ShineTypewriter-lgwzd.ttf")
+library(hexSticker)
+library(magick)
+
+fig = sticker(
+    subplot = "./infocaus/symbol.png",
+    s_x = 0.992,
+    s_y = 1.003,
+    s_width = 0.6,
+    s_height = 0.6,
+    package = "",
+    p_family = "ShineTypewriter",
+    p_size = 5.5,
+    p_color = ggplot2::alpha("#efd298",.95),#ggplot2::alpha("#3e3221",.75),
+    p_x = 1.00,
+    p_y = 0.585,
+    dpi = 300,
+    asp = 1,
+    h_size = 2.15,
+    h_color = ggplot2::alpha("#6d4b2c",.95),
+    h_fill = ggplot2::alpha('#efdab7',0.95),
+    white_around_sticker = F,
+    url = "https://stscl.github.io/infocaus",
+    u_color = ggplot2::alpha("#6d4b2c",.95),
+    u_size = 4.55,
+    filename = "./infocaus/infocaus.png"
+)
+
+# fig + ggview::canvas(1600,1800,units = "px",bg = "transparent") 
+
+# ggview::save_ggplot(fig + ggview::canvas(1600,1800,units = "px",bg = "transparent"),
+#                     "./infocausality/infocausality.png")
+
+image_read('./infocausality/infocausality.png') |> 
+  image_resize("240x278")|> 
+  image_write('./infocausality/infocausality_logo.png')
